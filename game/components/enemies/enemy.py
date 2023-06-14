da511a3e6e8d7bd8aaf2b931513e8cf88a8daf97
StevenBalaguera <Jerson.Balaguera@fundacion-jala.org>
2023-06-14 11:55:40 -0500

    Second-Class-Homework

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,30 +2,36 @@ import pygame
 import random
 
 from pygame.sprite import Sprite
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import ENEMY_1, ENEMY_2, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
-    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000, 1100]
+    X_POS_LIST = list(range(50, 1100, 50))
     Y_POS = 20
     SPEED_X = 5
     SPEED_Y = 4
     MOV_X = { 0: 'left', 1: 'right'}
+    IMAGE_DICT = { 0: ENEMY_1, 1: ENEMY_2}
 
     def __init__(self):
-        self.image = pygame.transform.scale(ENEMY_1, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
+        self.random_image = self.IMAGE_DICT[(random.randint(0, 1))]
+        self.image = pygame.transform.scale(self.random_image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.x = self.X_POS_LIST[(random.randint(0, 19))]
+        self.rect.x = self.X_POS_LIST[(random.randint(0, 20))]
         self.rect.y = self.Y_POS
         self.speed_x = self.SPEED_X
         self.speed_y = self.SPEED_Y
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
+        self.move_x_player_2 = random.randint(5, 15)
+        self.move_y_player_2 = random.randint(1, 8)
+
 
     def update(self, ships):
+        self.type_move_image()
         self.rect.y += self.speed_y
 
         if self.movement_x == 'left':
@@ -40,6 +46,12 @@ class Enemy(Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def type_move_image(self):
+        if self.random_image == ENEMY_2:
+            self.speed_y = self.SPEED_Y + self.move_y_player_2
+            self.speed_x = self.SPEED_X + self.move_x_player_2
+
 
     def change_movement_x(self):
         self.index += 1
