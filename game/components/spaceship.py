a3c6b9ee853836893c0032ffb1c9c4edd96eec3f
StevenBalaguera <Jerson.Balaguera@fundacion-jala.org>
2023-06-13 22:24:38 -0500

    Second-Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -27,18 +27,15 @@ class Spaceship(Sprite):
             self.move_down()
     
     def move_left(self):
-        if self.rect.left > 0:
-            self.rect.x -= 10
-
-        if self.rect.left == 0:
-            self.rect.x =+ 1050    
+        self.rect.x -= 10
+        if self.rect.left <= 0:
+            self.rect.x = SCREEN_WIDTH - self.SPACESHIP_WIDTH 
 
     def move_right(self):
-        if self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10
-
-        if self.rect.right == SCREEN_WIDTH:
-            self.rect.x -= 1050
+        self.rect.x += 10
+        if self.rect.right >= SCREEN_WIDTH:
+            self.rect.x = 0
+            
     def move_up(self):
         if self.rect.y > self.HALF_SCREEN_HEIGHT:
             self.rect.y -= 10
